08f46aa658615d55734484361c72a7fc8fda2b0a
sahadev234 <161486670+sahadev234@users.noreply.github.com>
2025-03-23 17:17:12 +0545

    Update game.js

--- game.js
+++ game.js
@@ -75,21 +75,23 @@ const showDraw= () => {
 
 };
 const checkWinner = () => {
-     for(let pattern of winPatterns){
-        let pos1Val =boxes[pattern[0]].innerText;
-        let pos2Val =boxes[pattern[1]].innerText;
-        let pos3Val =boxes[pattern[2]].innerText;
-     
-     if (pos1Val!=""&&pos2Val!=""&&pos3Val!=""){
-        if (pos1Val===pos2Val&&pos2Val===pos3Val){
-            console.log("Winner",pos1Val);
-            showWinner(pos1Val);
-        }
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
 
-     }
-   
-    
-    }  
+        if (pos1Val !== "" && pos1Val === pos2Val && pos2Val === pos3Val) {
+            console.log("Winner:", pos1Val);
+            showWinner(pos1Val);
+            return; // Stop checking further once a winner is found
+        }
+    }
+
+    // If no winner is found and all boxes are filled, declare a draw
+    if (counter === 9) {
+        showDraw();
+    }
 };
+
 newGame.addEventListener("click",resetBtn);
 reset.addEventListener("click",resetBtn);
